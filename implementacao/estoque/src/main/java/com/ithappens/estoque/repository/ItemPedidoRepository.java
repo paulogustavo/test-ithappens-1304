376b93484a1e1df857ec0dce7f6dc2017dbeb527
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2020-06-30 00:43:33 -0300

    adicionados metodos de busca a ItemPedidoRepository

--- implementacao/estoque/src/main/java/com/ithappens/estoque/repository/ItemPedidoRepository.java
+++ implementacao/estoque/src/main/java/com/ithappens/estoque/repository/ItemPedidoRepository.java
@@ -2,6 +2,19 @@ package com.ithappens.estoque.repository;
 
 import com.ithappens.estoque.model.ItemPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ItemPedidoRepository extends JpaRepository<ItemPedido, Long> {
+
+    @Query("select ip from ItemPedido ip " +
+            "join ip.pedidoEstoque pe " +
+            "join ip.statusItemPedido sip " +
+            "where pe.id = :idPedido " +
+            "and sip.id = 1")
+    List<ItemPedido> findAllComStatusAtivoPorPedidoId(Long idPedido);
+
+    List<ItemPedido> findAllByPedidoEstoqueId(Long idPedidoEstoque);
+
 }
