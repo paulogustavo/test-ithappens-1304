f24a6653a7852d77398d5e8ef27f493500ae2e5b
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2020-06-30 00:44:30 -0300

    adicionado metodo que busca por id de filial e id de produto em EstoqueRepository

--- implementacao/estoque/src/main/java/com/ithappens/estoque/repository/EstoqueRepository.java
+++ implementacao/estoque/src/main/java/com/ithappens/estoque/repository/EstoqueRepository.java
@@ -3,5 +3,9 @@ package com.ithappens.estoque.repository;
 import com.ithappens.estoque.model.Estoque;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
+
+    Optional<Estoque> findByFilialIdAndProdutoId(Long idFilial, Long idProduto);
 }
