1fbda6a85326aad5a8a418f51fd21b0b1811e2f6
Paulo Gustavo <paulogustavo.seap.ma@gmail.com>
2020-06-30 16:03:37 -0300

    correcoes em ItemPedidoService dentre elas adicionado metodo de verificacao se pedido ja foi finalizado ao salvar item

--- implementacao/estoque/src/main/java/com/ithappens/estoque/service/ItemPedidoService.java
+++ implementacao/estoque/src/main/java/com/ithappens/estoque/service/ItemPedidoService.java
@@ -8,6 +8,7 @@ import com.ithappens.estoque.repository.PedidoEstoqueRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,30 +20,47 @@ public class ItemPedidoService {
     private final PedidoEstoqueRepository pedidoEstoqueRepository;
     private final EstoqueRepository estoqueRepository;
 
+    @Transactional
     public ItemPedido salvar(ItemPedido itemPedido){
+        verificaSeOPedidoJaFoiFinalizado(itemPedido);
         verificaSeValoresDoItemPedidoJaSeEncontramNoPedido(itemPedido);
-        itemPedido = verificaQuantidadeNoEstoque(itemPedido);
+        verificaQuantidadeNoEstoque(itemPedido);
+
+        StatusItemPedido statusAtivo = new StatusItemPedido();
+        statusAtivo.setId(StatusItemPedido.ATIVO);
+        itemPedido.setStatusItemPedido(statusAtivo);
+
         return itemPedidoRepository.save(itemPedido);
 
+    }
+
+    private void verificaSeOPedidoJaFoiFinalizado(ItemPedido itemPedido){
+        if(itemPedido.getPedidoEstoque() != null && itemPedido.getPedidoEstoque().getId() != null){
+            Optional<PedidoEstoque> pedidoEstoqueOptional = pedidoEstoqueRepository.findById(itemPedido.getPedidoEstoque().getId());
+            if(pedidoEstoqueOptional.isPresent()){
+                List<ItemPedido> itemPedidoList = itemPedidoRepository.findAllByPedidoEstoqueId(pedidoEstoqueOptional.get().getId());
+                if(!itemPedidoList.isEmpty()
+                        && itemPedidoList.stream().anyMatch(item -> item.getStatusItemPedido().getId().equals(StatusItemPedido.PROCESSADO)))
+                    throw new EstoqueCustomException("Pedido já processado! Impossível adicionar novos itens");
+            } else throw new EstoqueCustomException("Pedido não encontrado");
+        } else throw new EstoqueCustomException("Informe o código do pedido");
     }
 
     private void verificaSeValoresDoItemPedidoJaSeEncontramNoPedido(ItemPedido itemPedido){
         List<ItemPedido> itemPedidoList = itemPedidoRepository
                 .findAllByPedidoEstoqueId(itemPedido.getPedidoEstoque().getId());
         if (itemPedidoList.stream()
-                .anyMatch(item -> item.getProduto().getId().equals(itemPedido.getProduto().getId()))
-                && !itemPedido.getStatusItemPedido().getId().equals(StatusItemPedido.CANCELADO)) {
+                .anyMatch(item -> itemPedido.getProduto().getId().equals(item.getProduto().getId())
+                && !item.getStatusItemPedido().getId().equals(StatusItemPedido.CANCELADO))) {
             throw new EstoqueCustomException("O produto já encontra-se na lista.");
         }
     }
 
-    private ItemPedido verificaQuantidadeNoEstoque(ItemPedido itemPedido){
+    private void verificaQuantidadeNoEstoque(ItemPedido itemPedido){
         Optional<PedidoEstoque> pedidoEstoqueOptional = pedidoEstoqueRepository
                 .findById(itemPedido.getPedidoEstoque().getId());
         if(pedidoEstoqueOptional.isPresent()){
-            if(pedidoEstoqueOptional.get().getTipoPedidoEstoque().getId().equals(TipoPedidoEstoque.ENTRADA))
-                return itemPedido;
-            else {
+            if(pedidoEstoqueOptional.get().getTipoPedidoEstoque().getId().equals(TipoPedidoEstoque.SAIDA)) {
                 //verificar se a quantidade disponível em estoque para o produto e para filial é compatível
                 Optional<Estoque> estoqueOptional = estoqueRepository
                         .findByFilialIdAndProdutoId(pedidoEstoqueOptional.get().getFilial().getId(), itemPedido.getProduto().getId());
@@ -51,7 +69,6 @@ public class ItemPedidoService {
                             || estoqueOptional.get().getQuantidade().equals(0))
                         throw new EstoqueCustomException("Quantidade em estoque insuficiente.");
                     itemPedido.setValorUnitario(estoqueOptional.get().getProduto().getValorUnitario());
-                    return itemPedido;
 
                 } else throw new EstoqueCustomException("Estoque inexistente.");
             }
